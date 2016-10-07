f7363f888b1707cf82dd12040ac630480c1538a7
Pawan Rawal <pawan@dgraph.io>
2016-10-08 00:16:00 +0530

    Add some concurrency. Get only latest file patches from the latest commit

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	_ "net/http/pprof"
 
 	"github.com/golang/lint"
 	"github.com/pawanrawal/dlint/patch"
@@ -35,20 +38,25 @@ type ghFileInfo struct {
 	Patch  string
 }
 
-func fetchFileInfo(prNum int) []ghFileInfo {
-	fi := make([]ghFileInfo, 0, 2)
-	resp, err := http.Get(fmt.Sprintf("%v/pulls/%d/files", *basePath, prNum))
+type commitInfo struct {
+	Sha   string       `json:"sha"`
+	Files []ghFileInfo `json:"files"`
+}
+
+func fetchFileInfo(sha string) []ghFileInfo {
+	ci := commitInfo{}
+	resp, err := http.Get(fmt.Sprintf("%v/commits/%s", *basePath, sha))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&fi)
+	err = decoder.Decode(&ci)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return fi
+	return ci.Files
 }
 
 func fetchFileContent(rawUrl string) []byte {
@@ -74,10 +82,9 @@ type LintError struct {
 	CommitId string `json:"commit_id"`
 }
 
-func publishComments(prNum int, le []LintError) {
+func publishComments(prNum int, pc chan LintError) {
 	// TODO - Check the same comment on the same line shouldn't be published twice.
-	// TODO - Publish comments on only those lines that changed in this iteration.
-	for _, e := range le {
+	for e := range pc {
 		if *debugMode {
 			fmt.Printf("File: %v, error: %v at position %v.\n", e.Path, e.Body, e.Position)
 			continue
@@ -119,6 +126,35 @@ func lintFile(fileName string) bool {
 	return true
 }
 
+func findErrors(file ghFileInfo, sha string, pc chan LintError, wg *sync.WaitGroup) {
+	defer wg.Done()
+	l := lint.Linter{}
+	c := fetchFileContent(file.RawUrl)
+	// TODO - Add support for multiple linters like govet, errcheck.
+	problems, err := l.Lint(file.Name, c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cl := patch.ChangedLines(file.Patch)
+	var pos int
+	var ok bool
+	for _, p := range problems {
+		if p.Confidence < 0.8 {
+			continue
+		}
+		// Check the problem line should be part of changed lines in this PR.
+		if pos, ok = cl[p.Position.Line]; !ok {
+			continue
+		}
+		pc <- LintError{
+			Path:     file.Name,
+			Position: pos,
+			Body:     p.Text,
+			CommitId: sha,
+		}
+	}
+}
+
 // handler for incoming webhook that is triggered when a PR is created/modified.
 func payloadHandler(w http.ResponseWriter, r *http.Request) {
 	// parse pull request number.
@@ -143,42 +179,23 @@ func payloadHandler(w http.ResponseWriter, r *http.Request) {
 	sha := d.PullRequest.Head.Sha
 
 	// get files in PR.
-	fi := fetchFileInfo(d.Number)
-	// run them through golint to find errors.
-	var le []LintError
-	l := lint.Linter{}
+	fi := fetchFileInfo(sha)
+	problemsCh := make(chan LintError, 100)
+
+	go publishComments(d.Number, problemsCh)
+	var wg sync.WaitGroup
 	for _, file := range fi {
 		if !lintFile(file.Name) {
 			continue
 		}
-
-		c := fetchFileContent(file.RawUrl)
-		// TODO - Add support for multiple linters like govet, errcheck.
-		problems, err := l.Lint(file.Name, c)
-		if err != nil {
-			log.Fatal(err)
+		if *debugMode {
+			fmt.Printf("File info %+v\n", file)
 		}
-		cl := patch.ChangedLines(file.Patch)
-		var pos int
-		var ok bool
-		for _, p := range problems {
-			if p.Confidence < 0.8 {
-				continue
-			}
-			// Check the problem line should be part of changed lines in this PR.
-			if pos, ok = cl[p.Position.Line]; !ok {
-				continue
-			}
-			le = append(le, LintError{
-				Path:     file.Name,
-				Position: pos,
-				Body:     p.Text,
-				CommitId: sha,
-			})
-		}
+		wg.Add(1)
+		go findErrors(file, sha, problemsCh, &wg)
 	}
-	// publish comments.
-	publishComments(d.Number, le)
+	wg.Wait()
+	close(problemsCh)
 }
 
 const (
